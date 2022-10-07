83ead4eb96e9b61529ff95199ac0b202a70543ef
awanafnan17 <113637866+awanafnan17@users.noreply.github.com>
2022-10-08 01:57:37 +0500

    Update rock paper scissor.py

--- rock paper scissor.py
+++ rock paper scissor.py
@@ -1,44 +1,48 @@
 import random
-
-def gameWin(comp, you):
-    if comp == you:
-        return None
-    elif comp == 'r':
-        if you == 'p':
-            return True
-        elif you == 's':
-            return False
-    elif comp == 'p':
-        if you == 's':
-            return True
-        elif you == 'r':
-            return False
-    elif comp == 's':
-        if you == 'r':
-            return True
-        elif you == 'p':
-            return False
-
-
-
-comp = print("Comp's turn: Rock(r) Paper(p) or Scissor(s)?")
-randNo = random.randint(1, 3)
-if randNo == 1:
-    comp = 'r'
-elif randNo == 2:
-    comp = 'p'
-elif randNo == 3:
-    comp = 's'
-you = input("Your turn: Rock(r) Paper(p) or Scissor(s)?")
-a = gameWin(comp, you)
-
-print(f"Computer chose {comp}")
-print(f"You chose {you}")
+while True:
+    def gameWin(comp, you):
+        if comp == you:
+            return None
+        elif comp == 'r':
+            if you == 'p':
+                return True
+            elif you == 's':
+                return False
+        elif comp == 'p':
+            if you == 's':
+                return True
+            elif you == 'r':
+                return False
+        elif comp == 's':
+            if you == 'r':
+                return True
+            elif you == 'p':
+                return False
 
 
-if a == None:
-    print("The game is a tie!")
-elif a == True:
-    print("You Win!")
-else:
-    print("You Lose!")
+
+    print("Press q to exit the game.")
+    comp = print("Comp's turn: Rock(r) Paper(p) or Scissor(s)?")
+    randNo = random.randint(1, 3)
+    if randNo == 1:
+        comp = 'r'
+    elif randNo == 2:
+        comp = 'p'
+    elif randNo == 3:
+        comp = 's'
+
+    you = input("Your turn: Rock(r) Paper(p) or Scissor(s)?")
+    if you == 'q':
+        exit()
+    a = gameWin(comp, you)
+
+    print(f"Computer chose {comp}")
+    print(f"You chose {you}")
+
+
+    if a == None:
+        print("The game is a tie!")
+    elif a == True:
+        print("You Win!")
+    else:
+        print("You Lose!")
